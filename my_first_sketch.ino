40ac9617eb67641f1a674dbaaf16e5a3416da126
1402jelorand <jeanfebxl@hotmail.fr>
2023-04-05 23:49:20 +0200

    init

---
+++ my_first_sketch.ino
@@ -0,0 +1,13 @@
+#include <ArduinoMqttClient.h>
+#include <WiFiNINA.h>
+#include "arduino_secrets.h"
+
+void setup() {
+  // put your setup code here, to run once:
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
